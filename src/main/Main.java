86544d9224a1a99dadb5541d88a6fb640bf6258b
Matus Minarovic <matusm44@gmail.com>
2018-11-13 23:13:31 +0000

    Edited user interface

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,46 +7,44 @@ import userinput.Input;
 public class Main {
 
     public static void main(String[] args){
-        Input in = new Input();
-        int n = in.getInt();
-        int[] values = in.getInts(n);
-        int newValue = in.getInt();
-        in.close();
+        Node root = initializeBT();
+        displayBT(root);
+    }
 
-        Node root = null;
-
-        for (int i = 0; i < n; i++) {
-            root = BinaryTree.insert(root, values[i]);
-        }
-        root = BinaryTree.insert(root, newValue);
-
+    public static void displayBT(Node root){
         displayInOrderBT(root);
         System.out.println();
         displayPreOrderBT(root);
     }
 
-    public static void displayInOrderBT(Node root){
+    public static Node initializeBT(){
+        Node root = null;
+        Input in = new Input();
+        int n = in.getInt();
+        int[] values = in.getInts(n);
+        in.close();
+        for (int i = 0; i < n; i++) {
+            root = BinaryTree.insert(root, values[i]);
+        }
 
+        return root;
+    }
+
+    public static void displayInOrderBT(Node root){
         if (root.left != null)
             displayInOrderBT(root.left);
-
         System.out.print(root.val + "\t");
-
         if (root.right != null)
             displayInOrderBT(root.right);
 
     }
 
     public static void displayPreOrderBT(Node root){
-
         System.out.print(root.val + "\t");
-
         if (root.left != null)
             displayPreOrderBT(root.left);
-
         if (root.right != null)
             displayPreOrderBT(root.right);
-
     }
 
 }
